a6e22636139c5fede6c668f5f699d8d6baccf149
znnBanana <2506326075@qq.com>
2019-10-04 10:13:03 +0800

    [znn]更新了ej-znn

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
 $(function(){
 	// 为导航添加事件
-	$("ul.left_nav").on("click","li",function(){
-		var url = $(this).attr("url");
-		$("ul.left_nav > li").removeClass("current");
+	$(".wrapper").load("./pages/customer.html");
+	$(".left ul li").click(function(){
 		$(this).addClass("current");
-		$("#wrapper").load(url);
+		$(this).siblings().removeClass("current");
+		if($(this).text() == "顾客管理"){
+			// console.log("./pages/customer.html");
+			$(".wrapper").load("./pages/customer.html");
+		}else if($(this).text() == "员工管理"){
+			$(".wrapper").load("./pages/waiter.html")
+		}else if($(this).text() == "产品管理"){
+			$(".wrapper").load("./pages/product.html");
+		}else if($(this).text() == "地址管理"){
+			$(".wrapper").load("./pages/address.html");
+		}else if($(this).text() == "栏目管理"){
+			$(".wrapper").load("./pages/category.html");
+		}else if($(this).text() == "评论管理"){
+			$(".wrapper").load("./pages/comment.html")
+		}
 	})
-
-	// 模拟点击第一个li
-	$("ul.left_nav > li:first-child").trigger("click");
-	
 });
